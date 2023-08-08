c91048ff6c245d86bc50a09b10a62c22daf2af42
mprakashkumar <141749689+mprakashkumar@users.noreply.github.com>
2023-08-08 20:12:42 +0530

    clone of netflix
    this is the project  .

---
+++ js/vishal.js
@@ -0,0 +1,10 @@
+function openForm() {
+  document.getElementById("bg-model").style.display = "block";
+}
+function myFunction() {
+  alert("Registered Successfully........");
+}
+function closeForm() {
+  document.getElementById("bg-model").style.display = "none";
+}
+
